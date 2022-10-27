75b09666cda8ffc2fc59a7c329b428681dcd6aad
Максим Калинин <maksim.kalinin1@gmail.com>
2022-10-27 13:54:50 +0300

    Added decorator

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ class PropertyownerInline(admin.TabularInline):
     model = Flat.flats_by.through
     raw_id_fields = ('owner', 'flat')
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address')
     readonly_fields = ('created_at',)
@@ -17,15 +17,11 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ('liked_by',)
     inlines = [PropertyownerInline,]
 
-
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
 
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
